81f5040ff11633f75e1062def5c395b2d9992254
Michael de Wit <mjwwit@gmail.com>
2017-03-28 22:14:05 +0200

    Remove node_modules and built sources and add testing dependencies

--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="node" />
-import { Server as NetServer } from 'net';
-export default class ProxifactAdapter {
-    private listener;
-    constructor();
-    listen(port: number): NetServer;
-}
---
+++ wallaby.js
@@ -0,0 +1,16 @@
+module.exports = function () {
+  return {
+    files: [
+      'src/**/*.ts'
+    ],
+
+    tests: [
+      'test/**/*.spec.ts'
+    ],
+
+    env: {
+      type: 'node',
+      runner: 'node'
+    }
+  };
+};
